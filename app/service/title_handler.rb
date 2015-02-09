914e5779d188b5768f0bb63b43612caa50d25ff0
Kate Lane <katelane@gmail.com>
2015-02-08 20:38:01 -0700

    links working, service added, title handler added

---
+++ app/service/title_handler.rb
@@ -0,0 +1,11 @@
+require 'open-uri'
+
+class TitleHandler
+  def self.find_title(url)
+    parse_title(Nokogiri::HTML(open(url)))
+  end
+
+  def self.parse_title(page)
+    page.search("title").text.gsub("\n", "").strip
+  end
+end
